0171ad86a6134fa53be9584029ee8dcf1d5baf10
Adameia Ramsey <adameiaproductions@gmail.com>
2024-03-21 11:59:56 -0500

    MVP review version - tested

--- src/components/pages/MyEvents.jsx
+++ src/components/pages/MyEvents.jsx
@@ -1,17 +1,19 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import { EventsList } from "../events/EventsList"
 import "./Pages.css"
 
 
 export const MyEvents = ({ currentUser }) => {
+    const navigate = useNavigate()
     const showUsersOwnedEvents = true
     return (
         <>
             <h2>Your owned events</h2>
             <div>
-                <li className="editprofile"> 
-                    <Link to={"/myevents/editprofile"}>Edit Profile</Link>
-                </li>
+                <button
+                    className="btn btn-primary"
+                    onClick={() => {navigate(`/myevents/editprofile`)}}
+                >Edit Profile</button>
             </div>
             <EventsList 
                 currentUser={currentUser}
